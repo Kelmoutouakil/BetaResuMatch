a7bdf3554b3059795e93146e5528f175165ef43d
abdo-ox <abderrahim.jari6@gmail.com>
2025-03-21 23:05:31 +0000

    push

--- backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import Upload, JDupload
-
-urlpatterns = [
-    path('upload/', Upload, name='upload'), 
-    path('JDupload/', JDupload, name='JDupload'), 
-    # Make sure the URL pattern is correct
-]
-
